803f8c751055876ef1cec7c422453b2e1fcaaefb
git <ahmedsalah511998@gmail.com>
2025-12-01 15:40:59 +0300

    yutyutyuty

--- dashboard/track_visit.php
+++ dashboard/track_visit.php
@@ -23,10 +23,13 @@ mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
 if (mysqli_num_rows($result) == 0) {
-    // أول مرة يزورها ⇒ سجل الزيارة
-    $sql = "INSERT INTO visitors (ip_address, user_agent, page_url) VALUES (?, ?, ?)";
+    // حساب الوقت الحالي + 3 ساعات
+    $visit_time = date('Y-m-d H:i:s', strtotime('+3 hours'));
+
+    // أول مرة يزورها ⇒ سجل الزيارة مع إضافة 3 ساعات
+    $sql = "INSERT INTO visitors (ip_address, user_agent, page_url, visit_time) VALUES (?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "sss", $ip, $user_agent, $page_url);
+    mysqli_stmt_bind_param($stmt, "ssss", $ip, $user_agent, $page_url, $visit_time);
     mysqli_stmt_execute($stmt);
 }
 ?>
